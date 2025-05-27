a7029128378e66fc32559c1f1c089d3d93dd8f6c
João Augait <49602784+augait@users.noreply.github.com>
2025-05-27 20:12:49 -0300

    Set up environment and dependencies

--- example/src/supabaseClient.ts
+++ example/src/supabaseClient.ts
@@ -1,7 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+// Get the URL and anon key from your project settings
+const supabaseUrl = 'YOUR_SUPABASE_URL';
+const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
